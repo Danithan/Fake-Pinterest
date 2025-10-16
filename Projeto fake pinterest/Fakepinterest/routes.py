2e1198e0507a692946e3f17eb85915cd53f7e9f7
Daniel Nathan <danielnathanleite@gmail.com>
2025-10-16 16:14:23 -0300

    feat: arquivos do projeto até aula 08
    homepage e perfil criados, bancos de dados feitos, e organização de pastas concluido

---
+++ Projeto fake pinterest/Fakepinterest/routes.py
@@ -0,0 +1,11 @@
+#criar as rotas do site
+from flask import render_template, url_for
+from Fakepinterest import app
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/perfil/<usuario>')
+def perfil(usuario):
+    return render_template('perfil.html', usuario=usuario)
