4397965557a8c09311495ddaecf49a9bc0988a74
Daniel Nathan <danielnathanleite@gmail.com>
2025-10-22 21:21:14 -0300

    feat:criacao dos formularios do pinterest

--- Projeto fake pinterest/Fakepinterest/routes.py
+++ Projeto fake pinterest/Fakepinterest/routes.py
@@ -2,7 +2,11 @@
 from flask import render_template, url_for
 from Fakepinterest import app
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 from flask_login import login_required
+=======
+from Fakepinterest.forms import FormLogin, FormCriaConta
+>>>>>>> Stashed changes
 =======
 from Fakepinterest.forms import FormLogin, FormCriaConta
 >>>>>>> Stashed changes
@@ -15,8 +19,11 @@ def homepage():
 @app.route('/criarconta', methods=['GET', 'POST'])
 def criarconta():
     return render_template('criarconta.html',form=FormCriaConta)
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
 
 @app.route('/perfil/<usuario>')
 @login_required
